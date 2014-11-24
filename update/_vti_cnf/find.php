3992c1dd0d89a8f6593f8a4839d10b5f968de0bb
Siva Raparla <raparlateja@gmail.com>
2014-11-24 14:45:02 -0500

    changed the folder name and deleted the update folder

--- update/_vti_cnf/find.php
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|04 Apr 2008 19:50:14 -0000
-vti_extenderversion:SR|5.0.2.4803
-vti_backlinkinfo:VX|newsite/events/july.htm newsite/events/august.htm newsite/events/june.htm newsite/events/october.htm newsite/events/april.htm newsite/construction.htm newsite/news_win.htm newsite/events/september.htm newsite/events/february.htm newsite/events/march.htm newsite/events/december.htm newsite/index.htm newsite/assistant_manager.htm newsite/events/november.htm newsite/events/january.htm newsite/events/may.htm
--- update/_vti_cnf/list.php
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|27 Mar 2008 21:31:55 -0000
-vti_extenderversion:SR|5.0.2.4803
-vti_backlinkinfo:VX|newsite/events/july.htm newsite/events/august.htm newsite/events/june.htm newsite/events/october.htm newsite/events/april.htm newsite/construction.htm newsite/news_win.htm newsite/events/september.htm newsite/events/february.htm newsite/events/march.htm newsite/events/december.htm newsite/index.htm newsite/assistant_manager.htm newsite/events/november.htm newsite/events/january.htm newsite/events/may.htm
